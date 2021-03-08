a64bb897ea01e2fc7aa0db9c84c7fdd9e75b52c2
user-74 <49914731+user-74@users.noreply.github.com>
2021-03-08 12:58:37 -0500

    initial commit

---
+++ examples.js
@@ -0,0 +1,10 @@
+// adds funloader.js library into the examples.html via self-invoking function
+// (function(){
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'funloader.js';
+//     document.getElementsByTagName('head')[0].appendChild(script);    
+// })();
+
+// test that i can run a funloader.js function with
+funloader();
---
+++ funloader.js
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------
+// funloader.js for CSC309 individual project winter 2021
+// by: Wilson Gao
+// utorid/markus: gaowilso
+// ----------------------------------------------------------------
+
+function funloader(){
+    // code for loading screen
+
+    // test that it's loaded in correctly, logs the current date.
+    console.log(
+        (new Date().getDate()).toString() + "/" +
+        (new Date().getMonth() + 1).toString() + "/" +
+        (new Date().getFullYear()).toString()
+    );
+
+    // code
+}
+
+// needs a 
