d50d522983636c74c384bb29d713a251d0ab311d
user-74 <49914731+user-74@users.noreply.github.com>
2021-03-23 17:56:32 -0400

    updated alpha release

--- pub/js/funloader.js
+++ pub/js/funloader.js
@@ -11,6 +11,7 @@ const timer = (ms) => new Promise((res) => setTimeout(res, ms))
 // JS Object, array of all active loading bars
 const activeProgressBars = []
 
+
 // Basic Loading Bar Functionality:
 
 // Creates the default loading bar
@@ -32,23 +33,34 @@ function defaultProgressBar() {
     progress.percentage = percentage
 
     // contains dom references to the progress bar and progress itself
-    let progressBarObject = {
-        progressBar: progressBar,
-        progress: progress,
-    }
+    activeProgressBars.push( {
+        HTMLreference: progressBar,
+        percentage: 0,
+        draggable: false,
+    })
 
     // add this progress bar to the JS array
-    activeProgressBars.push(progressBarObject)
+    log(activeProgressBars)
 
     return progressBar
 }
 
 async function addProgress(progressBar, amount) {
+    // get the element's properties from the list
+    let bar
+
+    for (bar of activeProgressBars) {
+        if (bar.HTMLreference == progressBar){
+            bar.percentage += amount
+            break
+        }
+    }
+
     // access the progress of the loading bar
     const progress = progressBar.firstElementChild
 
     // increment the loading bar by the specified amount
-    progress.percentage += amount
+    progress.percentage = bar.percentage
     progress.innerText = Math.round(progress.percentage * 100) / 100 + '%'
     progress.style.width = progress.percentage + '%'
 
@@ -60,14 +72,9 @@ async function addProgress(progressBar, amount) {
         await timer(2000)
 
         // remove the loading bar from the HTML DOM
+        delete activeProgressBars.splice(activeProgressBars.indexOf(bar), 1)
         progressBar.remove()
-        activeProgressBars.splice(
-            activeProgressBars.indexOf({
-                progressBar: progressBar,
-                progress: progress,
-            }),
-            1
-        )
+        log(activeProgressBars)
     }
 }
 
@@ -75,6 +82,14 @@ async function addProgress(progressBar, amount) {
 
 // Draggable Loading Bar:
 function makeDraggable(progressBar) {
+    // update draggable property
+    for (const bar of activeProgressBars) {
+        if (bar.HTMLreference == progressBar){
+            bar.draggable = true
+            break
+        }
+    }
+
     // Set the style of the loading bar so that it can be moved around on the page
     progressBar.style.cursor = 'move'
     progressBar.style.userSelect = 'none'
