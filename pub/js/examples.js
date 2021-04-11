f46301f0716f1630ce635bece2b452ae88ea3db1
user-74 <49914731+user-74@users.noreply.github.com>
2021-04-11 16:16:15 -0400

    updates to the progress bar

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -1,15 +1,22 @@
 // function to load a random image into the HTML DOM
+const timer = (ms) => new Promise((res) => setTimeout(res, ms))
+
 function createRandomImage(progressBar, amount) {
     const imageBox = document.querySelector('.image-box')
 
     const image = document.createElement('img')
     image.onload = function () {
-        addProgress(progressBar, amount)
+        progressBar.addProgress(amount)
     }
     image.src =
         'https://picsum.photos/200/300/?random&t=' + new Date().getTime()
 
     imageBox.appendChild(image)
+}
+
+function toggleAll() {
+    const progressBars = getAllActiveProgressBars()
+    progressBars.forEach((bar) => bar.toggleHidePercent())
 }
 
 // function that generates images and needs a loading bar
@@ -24,23 +31,25 @@ async function generateImages() {
     if (!Number.isInteger(num) || num <= 0) return
 
     // get a loading bar an
-    const progressBar = defaultProgressBar()
+    const p = new ProgressBar({opacity: 1, gradient:['red', 'green', 'blue']})
+    const progressBar = p.HTMLreference
     const placement = document.querySelector('.loading-box')
     placement.appendChild(progressBar)
 
     // makes the loading bar draggable
     if (draggable) {
-        makeDraggable(progressBar)
-        hidePercent(progressBar)
+        p.makeDraggable()
+
     } else {
         progressBar.style.margin = 'auto'
         progressBar.style.marginTop = '10px'
-        setProgressGradient(progressBar, '#FFFFFF', '#FFFFFF')
+        p.setFontSize('40px')
+        p.setProgressGradient(['#FFFFFF', '#FFFFFF'])
     }
 
     // generate images with a second delay between images, to randomize images
     for (let i = 0; i < num; i++) {
-        createRandomImage(progressBar, percent)
+        createRandomImage(p, percent)
         await timer(1000)
     }
 }
--- pub/js/funloader.js
+++ pub/js/funloader.js
@@ -4,17 +4,23 @@
 // utorid/markus: gaowilso
 // ----------------------------------------------------------------
 
-// to log things easily
-const log = console.log
-// timer for delaying animations etc. in async functions
-const timer = (ms) => new Promise((res) => setTimeout(res, ms))
 // JS Object, array of all active loading bars
 const activeProgressBars = []
 
 // Basic Loading Bar Functionality:
+/**
+ * Get all active loading bars
+ * @returns An array containing all active progress bars
+ */
+function getAllActiveProgressBars() {
+    return activeProgressBars
+}
 
-// Creates the default loading bar
-function defaultProgressBar() {
+/**
+ * This function generates the HTML for a progress bar
+ * @returns {HTMLDivElement} progressBar The HTML div containing the progress bar
+ */
+function progressBarHTML() {
     // create the divs that form the loading bar
     const progress = document.createElement('div')
     const progressBar = document.createElement('div')
@@ -26,138 +32,264 @@ function defaultProgressBar() {
     progressBar.className = 'progress-bar'
     progress.className = 'progress'
     outerPercentage.className = 'outerPercentage'
-    
+
     progressBar.appendChild(progress)
     progressBar.appendChild(outerPercentage)
-
-    // contains dom references to the progress bar and progress itself
-    activeProgressBars.push({
-        HTMLreference: progressBar,
-        percentage: 0,
-        draggable: false,
-        hidePercent: false,
-    })
-
     return progressBar
 }
+/**
+ * Options for the progress bar.
+ * @typedef {Object} Progress~Options
+ * @property {(number|string)} height - The height of the progress bar in pixels or some css amount
+ * @property {(number|string)} width - The width of the progress bar in pixels or some css amount
+ * @property {string} fontSize - The size of the percentage font, for both outer and inner percentages.
+ * @property {string} fontColor - The color of the percentage font, for both outer and inner percentages.
+ * @property {boolean} draggable - Whether the progress bar is draggable
+ * @property {boolean} hidePercent - Whether the percentage displayed in the progress bar is hidden
+ * @property {boolean} removeWhenDone - Whether to remove the progress bar when it reaches 100%
+ * @property {number} opacity - A number between 0 and 1 for the opacity of the progress bar
+ * @property {string[]} gradient - An array of colour values for a horizontal gradient.
+ * @property {boolean} overflow - Whether the progress bar can be filled beyond 100%
+ * @property {Object} image - An object containing an image to use as the background image for the progress bar
+ */
 
-// gets the progress bar element from the array of progress bars
-function getProgressBar(progressBar) {
-    return activeProgressBars.filter((object) => {
-        return object.HTMLreference === progressBar
-    })[0]
-}
+/** Class representing a progress bar */
+class ProgressBar {
+    /**
+     * Create a progress bar
+     * @param {...Progress~Options} options - The options for the progress bar
+     */
+    constructor(options = {}) {
+        this.percentage = 0
+        this.height = options.height || 30
+        this.width = options.width || 300
+        this.fontSize = options.fontSize || null
+        this.fontColor = options.fontColor || null
+        this.draggable = options.draggable || false
+        this.hidePercent = options.hidePercent || false
+        this.removeWhenDone = options.removeWhenDone || true
+        this.opacity = options.opacity || 1
+        this.gradient = options.gradient || ['#00adb981', '#00eeffec']
+        this.overflow = options.overflow || false
 
-// adds amount to the percentage of the progress bar
-function addProgress(progressBar, amount) {
-    // retrieve the progress bar's information from the stored array
-    const bar = getProgressBar(progressBar)
-    const prevPercent = bar.percentage
-    // update the percentage this bar has
-    bar.percentage += amount
-    // access the progress of the progress bar
-    const progress = progressBar.firstElementChild
-    const outerPercentage = progressBar.lastElementChild
+        this.HTMLreference = progressBarHTML()
+        this.HTMLprogress = this.HTMLreference.getElementsByClassName(
+            'progress'
+        )[0]
+        this.HTMLouterPercentage = this.HTMLreference.getElementsByClassName(
+            'outerPercentage'
+        )[0]
 
-
-    // increase progress by the specified amount
-    let percent = Math.round(bar.percentage * 100) / 100
-    // if we have over 100%, round it to 100%
-    if (percent >= 100) percent = 100
-    progress.style.width = percent + '%'
-
-    if (!bar.hidePercent) {
-        if (bar.percentage > 25){
-            progress.innerText = percent + '%'
-        } else{
-            outerPercentage.innerText = percent + '%'
+        if (options.image) {
+            this.image = {
+                source: options.image.source,
+            }
         }
-        if (prevPercent < 25 && bar.percentage > 25) {
-            outerPercentage.innerText = ''
+
+        this.style()
+
+        // contains dom references to the progress bar and progress itself
+        activeProgressBars.push(this)
+    }
+
+    /**
+     * Style the progress bar according to the options specified
+     */
+    style() {
+        this.setHeight(this.height)
+        this.setWidth(this.width)
+        if (this.fontSize) this.setFontSize(this.fontSize)
+        if (this.fontColor) this.setFontColor(this.fontColor)
+        this.setOpacity(this.opacity)
+        this.setProgressGradient(this.gradient)
+    }
+
+    /**
+     * Advances the progress bar by amount%.
+     * @param {number} amount - The percentage to add to the progress bar
+     */
+    addProgress(amount) {
+        if (typeof amount !== 'number') {
+            throw new Error('Amount to add to progress bar is invalid')
         }
-    }
 
-    // If we display 100%, loading has finished
-    if (percent === 100) {
-        finishBar(progressBar)
-    }
-}
+        this.percentage += amount
+        let percent = Math.round(this.percentage * 100) / 100
+        if (percent >= 100 && !this.overflow) percent = 100
 
-// function for animating the bar fading out after finished loading and deletes it
-async function finishBar(progressBar) {
-    const bar = getProgressBar(progressBar)
-    const progress = progressBar.firstElementChild
+        this.HTMLprogress.style.width = Math.min(100, percent) + '%'
+        this._updateInnerText()
 
-    // complete the bar
-    if (!bar.hidePercent) {
-        progress.innerText = '100%'
-    }
-    progress.style.width = '100%'
-
-    // Some css to make the loading bar appear to fade out after a while
-    await timer(1000)
-    progressBar.classList.add('fadeOut')
-    await timer(2000)
-
-    // remove the loading bar from the HTML DOM
-    delete activeProgressBars.splice(activeProgressBars.indexOf(bar), 1)
-    progressBar.remove()
-}
-
-// function if loading is cancelled
-function cancelBar(progressBar) {
-    const bar = getProgressBar(progressBar)
-    delete activeProgressBars.splice(activeProgressBars.indexOf(bar), 1)
-    progressBar.remove()
-}
-
-// Customization
-
-// do not show percentage inside progress bar
-function hidePercent(progressBar) {
-    const bar = getProgressBar(progressBar)
-    bar.hidePercent = true
-}
-
-// show percentage inside progress bar
-function unhidePercent(progressBar) {
-    const bar = getProgressBar(progressBar)
-    bar.hidePercent = false
-}
-
-// customize the gradient colors of the progress bar
-function setProgressGradient(progressBar, leftColor, rightColor) {
-    // retrieve the progress bar's information from the stored array
-    const progress = progressBar.firstElementChild
-    progress.style.backgroundImage =
-        'linear-gradient(to right,' + leftColor + ',' + rightColor + ')'
-    progress.style.boxShadow =
-        '0px 2px 2px -5px' + leftColor + ', 0px 2px 5px' + rightColor
-
-    console.log(progress)
-}
-
-// Interactivity:
-
-// Draggable Loading Bar:
-function makeDraggable(progressBar) {
-    // update draggable property
-    for (const bar of activeProgressBars) {
-        if (bar.HTMLreference == progressBar) {
-            bar.draggable = true
-            break
+        if (percent === 100 && this.removeWhenDone) {
+            this.finishBar()
         }
     }
 
-    // Set the style of the loading bar so that it can be moved around on the page
-    progressBar.style.cursor = 'move'
-    progressBar.style.userSelect = 'none'
-    progressBar.style.position = 'absolute'
-    progressBar.style.removeProperty('margin')
-    progressBar.style.removeProperty('margin-top')
+    /**
+     * Sets the progress bar to amount %
+     * @param {number} amount - The percentage to set the progress bar to
+     */
+    setProgress(amount) {
+        if (typeof amount !== 'number') {
+            throw new Error('Amount to set progress bar to is invalid')
+        } else if (amount < 0 || (amount > 100 && !this.overflow)) {
+            throw new Error('Amount to set progress bar to is out of range')
+        }
 
-    // Add a listener for mouse clicks
-    progressBar.addEventListener('mousedown', mouseDownHandler)
+        this.percentage = amount
+
+        this.HTMLprogress.style.width = Math.min(100, this.percentage) + '%'
+        this._updateInnerText()
+
+        if (percent === 100 && this.removeWhenDone) {
+            this.finishBar()
+        }
+    }
+
+    /**
+     * Helper function that updates the percentage that is shown or hidden
+     */
+    _updateInnerText() {
+        let percent = Math.round(this.percentage * 100) / 100
+        if (percent >= 100 && !this.overflow) percent = 100
+
+        if (!this.hidePercent) {
+            if (this.percentage > 50) {
+                this.HTMLprogress.innerText = percent + '%'
+                this.HTMLouterPercentage.innerText = ''
+            } else {
+                this.HTMLouterPercentage.innerText = percent + '%'
+            }
+        } else {
+            this.HTMLouterPercentage.innerText = ''
+            this.HTMLprogress.innerText = ''
+        }
+    }
+
+    async finishBar() {
+        // complete the bar
+        if (!this.hidePercent) {
+            this.HTMLprogress.innerText = '100%'
+        }
+        this.HTMLprogress.style.width = '100%'
+
+        // Some css to make the loading bar appear to fade out after a while
+        this.HTMLreference.classList.add('fadeOut')
+
+        // remove the loading bar from the HTML DOM
+        setTimeout(() => this.cancelBar(), 2000)
+    }
+
+    cancelBar() {
+        delete activeProgressBars.splice(activeProgressBars.indexOf(this), 1)
+        this.HTMLreference.remove()
+    }
+
+    setHeight(height) {
+        if (typeof height === 'number') {
+            this.height = height
+            this.HTMLreference.style.height = height + 'px'
+        } else if (typeof height === 'string') {
+            this.height = height
+            this.HTMLreference.style.height = height
+        } else {
+            throw new Error('Invalid height supplied to progress bar')
+        }
+    }
+
+    setOpacity(opacity) {
+        if (typeof opacity === 'number') {
+            this.opacity = opacity
+            this.HTMLreference.style.opacity = opacity
+        } else {
+            throw new Error('Invalid opacity supplied to progress bar')
+        }
+    }
+
+    setWidth(width) {
+        if (typeof width === 'number') {
+            this.width = width
+            this.HTMLreference.style.width = width + 'px'
+        } else if (typeof width === 'string') {
+            this.width = width
+            this.HTMLreference.style.width = width
+        } else {
+            throw new Error('Invalid width supplied to progress bar')
+        }
+    }
+
+    setFontSize(fontSize) {
+        if (typeof fontSize === 'string') {
+            this.fontSize = fontSize
+            this.HTMLprogress.style.fontSize = fontSize
+            this.HTMLouterPercentage.style.fontSize = fontSize
+        } else if (typeof fontSize === 'number') {
+            this.fontSize = fontSize + 'px'
+            this.HTMLprogress.style.fontSize = fontSize + 'px'
+            this.HTMLouterPercentage.style.fontSize = fontSize + 'px'
+        } else {
+            throw new Error('Invalid fontSize supplied to progress bar')
+        }
+    }
+
+    setFontColor(fontColor) {
+        if (typeof fontSize === 'string') {
+            this.HTMLprogress.style.fontSize = fontSize
+            this.HTMLouterPercentage.style.fontSize = fontSize
+        } else if (typeof fontSize === 'number') {
+            this.HTMLprogress.style.fontSize = fontSize + 'px'
+            this.HTMLouterPercentage.style.fontSize = fontSize + 'px'
+        } else {
+            throw new Error('Invalid fontSize supplied to progress bar')
+        }
+    }
+
+    setProgressGradient(colors) {
+        this.gradient = colors
+        console.log(this.gradient)
+        // retrieve the progress bar's information from the stored array
+        this.HTMLprogress.style.background =
+            'linear-gradient(to right, ' + colors.join(', ') + ' )'
+        this.HTMLprogress.style.boxShadow =
+            '0px 2px 2px -5px ' + colors[0] + ', 0px 2px 5px ' + colors[colors.length - 1]
+    }
+
+    // do not show percentage inside progress bar
+    hidePercent() {
+        this.hidePercent = true
+        this._updateInnerText()
+    }
+
+    // show percentage inside progress bar
+    unhidePercent() {
+        this.hidePercent = false
+        this._updateInnerText()
+    }
+
+    // toggle percentage showing
+    toggleHidePercent() {
+        this.hidePercent = !this.hidePercent
+        this._updateInnerText()
+    }
+
+    keepWhenDone() {
+        this.removeWhenDone = false
+    }
+
+    // Interactivity:
+
+    makeDraggable() {
+        // update draggable property in the object
+        this.draggable = true
+
+        // Set the style of the loading bar so that it can be moved around on the page
+        this.HTMLreference.style.cursor = 'move'
+        this.HTMLreference.style.userSelect = 'none'
+        this.HTMLreference.style.position = 'absolute'
+
+        // Add a listener for mouse clicks
+        this.HTMLreference.addEventListener('mousedown', mouseDownHandler)
+    }
 }
 
 // The current position of mouse and the selected progress bar
@@ -175,7 +307,7 @@ function mouseDownHandler(e) {
     active = e.target
 
     // in case the user clicks on the progress bar and not the container
-    if (active.className === 'progress') active = active.parentElement
+    if (active.className !== 'progress-bar') active = active.parentElement
 
     // mouse listeners
     document.addEventListener('mousemove', mouseMoveHandler)
